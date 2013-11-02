e40e3bd85de5ed0f301af3686240de0ff57f95f1
nathanallen <thenathanator@gmail.com>
2013-11-02 16:48:24 -0700

    js blinking cursor effect

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -39,8 +39,20 @@ function daysSinceStarting() {
   }
 }
 
+function recursiveBlinkingCursor(elem){
+  $elem = elem || $('#blinking-cursor')
+  setTimeout(function(){
+    $elem.attr("style", "visibility:hidden");
+    setTimeout(function(){
+      $elem.attr("style", "visibility:true");
+      recursiveBlinkingCursor($elem)
+    }, 600)
+  }, 600)
+}
+
 $('document').ready(function() {
   daysSinceStarting()
+  recursiveBlinkingCursor()
   testFeatures();
   addCodeLineNumbers();
 });
